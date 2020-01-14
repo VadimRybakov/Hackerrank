f7a23e1599d39b19b422daa42176414bab0c13a1
Vadim Rybakov <58461130+VadimRybakov@users.noreply.github.com>
2020-01-14 11:12:18 +0300

    Develop (#5)
     alternating characters

---
+++ src/Strings/AlternatingCharacters.java
@@ -0,0 +1,16 @@
+package Strings;
+
+public class AlternatingCharacters {
+    static int alternatingCharacters(String s) {
+        int count = 0;
+        for(int i = 0; i < s.length() - 1; i++){
+            if(s.charAt(i) == s.charAt(i + 1)) count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        String s = "ABABBAABBBA";
+        System.out.println(alternatingCharacters(s));
+    }
+}
